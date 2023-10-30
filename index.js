24ed8914f8d90a55f07e05a867af96708405e1b7
Jason <jasonbohio@gmail.com>
2023-10-30 15:04:44 -0400

    snark

--- index.js
+++ index.js
@@ -72,6 +72,9 @@ app.get('/callback', async (req, res) => {
     res.json(response);
 });
 
+app.get('/', (req, res) => {
+    res.send('Open the pod bay doors.');
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server listening on port ${process.env.PORT || 3000}`);
